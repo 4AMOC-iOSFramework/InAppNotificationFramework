5dcfef09e3abc635ace1d0286efe1191b7c110a8
hGxL <henri.m.gil@gmail.com>
2018-01-31 17:17:17 +0100

    Added TEST isUrl()

--- InAppNotificationFrameworkTests/InAppNotificationFrameworkTests.swift
+++ InAppNotificationFrameworkTests/InAppNotificationFrameworkTests.swift
@@ -11,6 +11,29 @@ import XCTest
 
 class InAppNotificationFrameworkTests: XCTestCase {
     
+    let notificationView = NotificationView()
+    
+    func test_bad_isUrl(){
+        let b = notificationView.isUrl(urlString: "fezf")
+        XCTAssertEqual(b, false)
+    }
+    
+    func test_http_isUrl(){
+        let b = notificationView.isUrl(urlString: "http://facebook.com")
+        XCTAssertEqual(b, true)
+    }
+    
+    func test_https_isUrl(){
+        let b = notificationView.isUrl(urlString: "https://facebook.com")
+        XCTAssertEqual(b, true)
+    }
+    
+    func test_empty_isUrl(){
+        let b = notificationView.isUrl(urlString: "")
+        XCTAssertEqual(b, false)
+    }
+    
+    
     func testZero(){
         XCTAssertEqual(0.isPair, true)
     }
